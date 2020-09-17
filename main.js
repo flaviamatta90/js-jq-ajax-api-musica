7850e51b9668d8fe75e22d5f1d17d9c7f25b0607
Flavia <flaviamatta90@gmail.com>
2020-09-17 15:19:34 +0200

    try

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
 $(document).ready(function() {
-	//Code
+
+  // chiamata ajax
+  $.ajax(
+  {
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    "method": "GET",
+    "success": function (data, stato) {
+      var rispostaServer = data.response;
+      console.log(data.response);
+      musica(response);
+
+    },
+    "error": function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+
+  });
+
+  function musica(){
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    for(var i = 0; i < response.length; i++){
+
+      var music = music[i];
+
+      var context = {
+        "poster" : music.poster,
+        "title" : music.title,
+        "author" : music.author,
+        "genre" : music.genre,
+        "year" : music.year
+      };
+
+      var html = template(context);
+
+      $('#add-cd').append(html);
+    }
+  };
+
+
+
 });
+
+
+
+
+// "poster": "https://images.pyramidshop.com/images/_popup/ACPPR48056.jpg",
+// "title": "Live at Wembley 86",
+// "author": "Queen",
+// "genre": "Pop",
+// // "year": "1992"
